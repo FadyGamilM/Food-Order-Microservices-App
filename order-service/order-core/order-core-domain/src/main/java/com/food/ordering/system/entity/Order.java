dadcca3bb3e4ed9f3f4aaeff9df3ede37049de9b
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-06-04 00:22:45 +0300

    FEATURE | Order-Service | finish the order-processing-aggregate [domain layer]

--- order-service/order-core/order-core-domain/src/main/java/com/food/ordering/system/entity/Order.java
+++ order-service/order-core/order-core-domain/src/main/java/com/food/ordering/system/entity/Order.java
@@ -1,17 +1,91 @@
 package com.food.ordering.system.entity;
 
 import com.food.ordering.system.domain.entity.AggregateRoot;
-import com.food.ordering.system.domain.valueobject.CustomerID;
-import com.food.ordering.system.domain.valueobject.Money;
-import com.food.ordering.system.domain.valueobject.OrderID;
-import com.food.ordering.system.domain.valueobject.ResturantID;
+import com.food.ordering.system.domain.valueobject.*;
 import com.food.ordering.system.valueobject.StreetAddress;
 import com.food.ordering.system.valueobject.TrackingID;
 
+import javax.sound.midi.Track;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order extends AggregateRoot<OrderID> {
+
+    public static class Builder {
+        private OrderID orderID;
+        private CustomerID customerID;
+        private ResturantID resturantID;
+        private TrackingID trackingID;
+        private Money totalPrice;
+        private List<OrderItem> orderItems;
+        private List<String> failureMsgs;
+        private OrderStatus status;
+        private StreetAddress streetAddress;
+
+        public Builder orderID(OrderID val) {
+            this.orderID = val;
+            return this;
+        }
+
+        public Builder resturantID(ResturantID val) {
+            this.resturantID = val;
+            return this;
+        }
+
+        public Builder customerID(CustomerID val) {
+            this.customerID = val;
+            return this;
+        }
+
+        public Builder trackingID(TrackingID val) {
+            this.trackingID = val;
+            return this;
+        }
+
+
+        public Builder totalPrice(Money val) {
+            this.totalPrice = val;
+            return this;
+        }
+
+
+        public Builder failureMsgs(List<String> val) {
+            this.failureMsgs = val;
+            return this;
+        }
+
+        public Builder status(OrderStatus val) {
+            this.status = val;
+            return this;
+        }
+
+        public Builder orderItems(List<OrderItem> val) {
+            this.orderItems = val;
+            return this;
+        }
+
+        public Builder streetAddress(StreetAddress val) {
+            this.streetAddress = val;
+            return this;
+        }
+
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
+
+    private Order(Builder builder) {
+        super.setId(builder.orderID);
+        this.customerId = builder.customerID;
+        this.resturantID = builder.resturantID;
+        this.trackingId = builder.trackingID;
+        this.orderItems = builder.orderItems;
+        this.totalPrice = builder.totalPrice;
+        this.failureMsgs = builder.failureMsgs;
+        this.status = builder.status;
+        this.streetAddress = builder.streetAddress;
+    }
 
     private final CustomerID customerId;
     private final ResturantID resturantID;
@@ -23,6 +97,10 @@ public class Order extends AggregateRoot<OrderID> {
     private final Money totalPrice;
 
     // each order has a lsit of order items
-    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+    private final List<OrderItem> orderItems;
+
+    private OrderStatus status; // not final because it will change through the life-cycle of the app
+
+    private List<String> failureMsgs;
 
 }
--- order-service/order-core/order-core-domain/src/main/java/com/food/ordering/system/valueobject/TrackingID.java
+++ order-service/order-core/order-core-domain/src/main/java/com/food/ordering/system/valueobject/TrackingID.java
@@ -4,6 +4,7 @@ import com.food.ordering.system.domain.valueobject.BaseID;
 
 import java.util.UUID;
 
+// the TrackingID will be returned to the client so he can track the order via the Get endpoint
 public class TrackingID extends BaseID<UUID> {
     public TrackingID(UUID val) {
         super(val);

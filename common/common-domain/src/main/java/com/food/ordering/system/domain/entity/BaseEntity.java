cb289710bbaa832a05765f2e906ee2e12e33e51e
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-06-03 10:46:26 +0200

    FEATURE | implement the Common base package for reusability in Core.Domain project

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.food.ordering.system.domain.entity;
+
+import java.util.Objects;
+
+// i will extend this BaseEntity class into all other entites in different domains, and replace the generic ID type with the
+// custom identifier type of each domain entity
+public abstract class BaseEntity<ID> {
+    ID id;
+
+    // getters and setters
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    // essential methods to compare two entities togeather
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity<?> that = (BaseEntity<?>) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
